5b183175b44c79dbfa06985616aadb8321a77ad1
weyewe <w.yunnal@gmail.com>
2011-09-25 17:18:15 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Neomagank::Application.config.secret_token = 'f9c88a0b39ae3d957c034942afcd2ae871c92637b718c07afc20c58484ef79412e2c6752d8e96b338cb2a5196bcfb7eb5fd629c1f915f1f9d7d5d24cd5d84597'
